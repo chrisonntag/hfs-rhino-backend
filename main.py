90dd94b04542e0204278d9c94c60df373fad3599
chrisonntag <sonntagc@fim.uni-passau.de>
2017-09-16 10:17:03 +0200

    Add basic route /rhino/update delivering instructions based on location

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
 import sys
 import traceback
 from flask import Flask, request, jsonify
+from controllers.theweathercompany import WeatherRegion
+from controllers.knowledge import KnowledgeBase
 
 app = Flask(__name__)
+version = 'v1.0'
+project_name = 'rhino'
+pre = '/%s' % project_name
 
 
 @app.route('/')
 def index():
-    return 'Welcome to Rhino.'
+    return jsonify({'message': 'Welcome to Rhino.', 'version': version, 'base_url': '%s/' % pre})
 
 
-@app.route('/update', methods=['POST'])
+@app.route('%s/update' % pre, methods=['POST'])
 def update():
     try:
         lat = request.form['latitude']
         lng = request.form['longitude']
-        return jsonify({'lat': lat, 'lng': lng})
+        region = WeatherRegion(lat, lng)
+
+        if region.is_endangered():
+            data = region.parse()
+            db = KnowledgeBase()
+            return jsonify({
+                'endangered': True,
+                'type': data['type'],
+                'level': data['level'],
+                'instructions': db.get_instructions(data)
+            })
+        else:
+            return jsonify({'endangered': False})
     except Exception as ex:
         return jsonify({'error': str(ex), 'trace': traceback.format_exc()})
 
